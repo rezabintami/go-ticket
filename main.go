f49ae5dab51aa202b33b8b238d585247768a02cd
rezabintami <rezabintami@gmail.com>
2021-08-06 11:09:15 +0700

    add ticket feature controller and model

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"net/http"
+	"ticketing/controller/cTickets"
 	"ticketing/controller/cTopup"
 	"ticketing/controller/cUsers"
 )
 
 func main() {
+	//! TOPUP
 	http.HandleFunc("/topup", cTopup.PostTopUpPayment)
+
+	//! USERS
 	http.HandleFunc("/users", cUsers.GetProfile)
 	http.HandleFunc("/users", cUsers.UpdateProfile)
+
+	//! Ticket
+	http.HandleFunc("/tickets", cTickets.PostTicket)
+	http.HandleFunc("/tickets", cTickets.CancelTicket)
+
+	//! AUTH
 	http.HandleFunc("/login", cUsers.Login)
+
 	http.ListenAndServe(":8000", nil)
 }
