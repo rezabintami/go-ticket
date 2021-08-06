8761edb89d9edacc3a4e45463c8b08a9361af04a
rezabintami <rezabintami@gmail.com>
2021-08-06 10:40:46 +0700

    add post topup

---
+++ model/TopUp/topup.go
@@ -0,0 +1,13 @@
+package topup
+
+import (
+	"time"
+)
+
+type TopUp struct {
+	ID         int       `json:"id"`
+	User_ID    int       `json:"user_id"`
+	Balance    float64   `json:"balance"`
+	Created_At time.Time `json:"created_at"`
+	Updated_At time.Time `json:"updated_at"`
+}
