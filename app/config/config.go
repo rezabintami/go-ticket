6d6a9a2eb8911cf0f4d8e96a43167090491f8927
Muhammad Reza Bintami <muhammad.bintami@amartha.com>
2021-09-14 15:08:32 +0700

    fix bug configure

--- app/config/config.go
+++ app/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"os"
+
 	"github.com/spf13/viper"
 )
 
@@ -43,17 +45,29 @@ func GetConfig() Config {
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		viper.BindEnv(conf.MYSQL_DB_HOST)
-		viper.BindEnv(conf.MYSQL_DB_PORT)
-		viper.BindEnv(conf.MYSQL_DB_USER)
-		viper.BindEnv(conf.MYSQL_DB_PASS)
-		viper.BindEnv(conf.MYSQL_DB_NAME)
+		// viper.BindEnv(conf.MYSQL_DB_HOST)
+		// viper.BindEnv(conf.MYSQL_DB_PORT)
+		// viper.BindEnv(conf.MYSQL_DB_USER)
+		// viper.BindEnv(conf.MYSQL_DB_PASS)
+		// viper.BindEnv(conf.MYSQL_DB_NAME)
 
-		viper.BindEnv(GetConfig().FACEBOOK_AUTH_CLIENT)
-		viper.BindEnv(GetConfig().FACEBOOK_AUTH_SECRET)
+		// viper.BindEnv(conf.FACEBOOK_AUTH_CLIENT)
+		// viper.BindEnv(conf.FACEBOOK_AUTH_SECRET)
 
-		viper.BindEnv(GetConfig().GOOGLE_AUTH_CLIENT)
-		viper.BindEnv(GetConfig().GOOGLE_AUTH_SECRET)
+		// viper.BindEnv(conf.GOOGLE_AUTH_CLIENT)
+		// viper.BindEnv(conf.GOOGLE_AUTH_SECRET)
+		
+		conf.MYSQL_DB_HOST = os.Getenv("MYSQL_DB_HOST")
+		conf.MYSQL_DB_PORT = os.Getenv("MYSQL_DB_PORT")
+		conf.MYSQL_DB_USER = os.Getenv("MYSQL_DB_USER")
+		conf.MYSQL_DB_PASS = os.Getenv("MYSQL_DB_PASS")
+		conf.MYSQL_DB_NAME = os.Getenv("MYSQL_DB_NAME")
+
+		conf.FACEBOOK_AUTH_CLIENT = os.Getenv("FACEBOOK_AUTH_CLIENT")
+		conf.FACEBOOK_AUTH_SECRET = os.Getenv("FACEBOOK_AUTH_SECRET")
+
+		conf.GOOGLE_AUTH_CLIENT = os.Getenv("GOOGLE_AUTH_CLIENT")
+		conf.GOOGLE_AUTH_SECRET = os.Getenv("GOOGLE_AUTH_SECRET")
 	}
 
 	if err := viper.Unmarshal(&conf); err != nil {
