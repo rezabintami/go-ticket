2aae039d61a446bbfbe6cafd5e288ac203435a21
Muhammad Reza Bintami <muhammad.bintami@amartha.com>
2021-09-14 13:05:26 +0700

    setting server

--- app/main.go
+++ app/main.go
@@ -101,8 +101,8 @@ func main() {
 	routesInit.RouteRegister(e)
 
 	port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port =configApp.SERVER_PORT
-	// }
+	if port == "" {
+		port = "8000"
+	}
 	log.Fatal(e.Start(":" + port))
 }
